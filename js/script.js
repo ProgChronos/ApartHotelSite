0438af2dc897ece2f26051d609698947f8821182
Criativochronos <criativochronos@gmail.com>
2023-06-14 09:54:23 -0300

    First commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-/*-REVELAR O CAMPO PESQUISA-*/
-{
-    function onSearch() {
-        const onFind = document.getElementById('searchInput');
-
-        onFind.innerHTML = `<input type="text" id="myInput" placeholder=" Digite e pressione enter..." style="display: block;">`;
-    }
-    function offSearch() {
-        const offFind = document.getElementById('searchInput');
-
-        offFind.innerHTML = `<input type="text" id="myInput" style="display: none;">`;
-    }
-}
 
 /*-REVELAR INFO DOS PROJETOS-*/
 {
