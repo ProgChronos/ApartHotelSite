706b54b93a6ab9a7114d22f6965ec9d9095e4fa3
Alisson Santos Pinho <alisson.pinho@chronos.local>
2023-06-19 14:09:47 -0300

    Componentized main elements of the page

---
+++ js/head.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    (function head() {
+        const head = document.querySelector('.head');
+
+        const headInjet = `
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Xclusive</title>
+      <link rel="shortcut icon" href="img/logo-branco-1.png" type="image/x-icon">
+      <!--BOOTSTRAP CSS-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+      <!--CSS LOCAL-->
+      <link rel="stylesheet" href="/css/style.css">
+      <!--CSS SLICK SLIDER-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css">
+      
+      `;
+
+        head.innerHTML = headInjet;
+    })();
+});
---
+++ js/header.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    (function header() {
+      const header = document.querySelector('.header');
+  
+      const headerInjet = `
+      <nav class="navbar navbar-dark  fixed-top">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img class="nav-logo" src="img/logo-branco-1.png" alt="Logo"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
+              aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+              aria-labelledby="offcanvasDarkNavbarLabel">
+              <div class="offcanvas-header">
+                  <img class="nav-logo" src="img/logo-branco-1.png" alt="Logo">
+                  <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                      aria-label="Close"></button>
+              </div>
+              <div class="offcanvas-body">
+                  <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                      <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Galeria</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                              aria-expanded="false">
+                              Espaços
+                          </a>
+                          <ul class="dropdown-menu dropdown-menu-dark">
+                              <li><a class="dropdown-item" href="#">Apartamento-1</a></li>
+                              <li><a class="dropdown-item" href="#">Apartamento-2</a></li>
+                              <li><a class="dropdown-item" href="#">Serviços</a></li>
+                              <li><a class="dropdown-item" href="#">Boate</a></li>
+                          </ul>
+                      </li>
+                  </ul>
+                  <form class="d-flex mt-3" role="search">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-success" type="submit">Search</button>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </nav>
+      `;
+  
+      header.innerHTML = headerInjet;
+    })();
+  });
+  
