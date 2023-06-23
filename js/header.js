a3a1c7fff5b0aed1b03958f95653692f2f64e194
Criativochronos <criativochronos@gmail.com>
2023-06-23 08:31:41 -0300

    Inserted index.html link on navbar-brand.

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const headerInjet = `
       <nav class="navbar navbar-dark  fixed-top">
       <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img class="nav-logo" src="img/logo-branco-1.png" alt="Logo"></a>
+          <a class="navbar-brand" href="index.html"><img class="nav-logo" src="img/logo-branco-1.png" alt="Logo"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
               data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
               aria-label="Toggle navigation">
