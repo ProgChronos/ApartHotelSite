1cbd350056d8e365a8b6cf65f04f399b356738cb
Criativochronos <criativochronos@gmail.com>
2023-06-27 13:55:34 -0300

    Inserted dinamic class and logo footer

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     (function footer() {
-        const footer = document.querySelector('#footer');
+        const footer = document.querySelector('footer');
+        //Adição de classes ao footer
+        footer.classList.add('d-flex', 'flex-wrap', 'justify-content-between', 'align-items-center', 'py-3', 'my-4', 'border-top');
+
 
         const footerInjet = `
             
@@ -9,9 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     <a href="/"
         class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32">
-            <use xlink:href="#bootstrap"></use>
-        </svg>
+        <img class="nav-logo" src="img/logo-branco-1.png" alt="Logo">
     </a>
 
     <ul class="nav col-md-4 justify-content-end">
