ab4734865ad0e1d79669ffe289925591f794a072
Felipe Lima <programacaochronos@gmail.com>
2023-07-20 16:08:45 -0300

    Changed HD gallery lib.

--- js/head.js
+++ js/head.js
@@ -14,12 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
       <!--BOOTSTRAP CSS-->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+          <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/css/uikit.min.css" />
       <!--CSS LOCAL-->
       <link rel="stylesheet" href="/css/style.css">
       <!--CSS SLICK SLIDER-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css">
-      
       `;
 
         head.innerHTML = headInjet;
